25998779e791804060408d4582badbfc4dc2456f
abneeeees <avneeshsingh288@gmail.com>
2026-09-05 02:09:10 +0530

    FIX: Implementation of conditional statement that decides which rate limiting algorithm to call

--- src/deepthrottle/services/ratelimiter_services.py
+++ src/deepthrottle/services/ratelimiter_services.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
-from deepthrottle.algorithms.leaky_bucket import LeakyBucket
-from deepthrottle.algorithms.sliding_window_counter import SlidingWindowCounter
-from deepthrottle.algorithms.token_bucket import TokenBucket
+from src.deepthrottle.algorithms.leaky_bucket import LeakyBucket
+from src.deepthrottle.algorithms.sliding_window_counter import SlidingWindowCounter
+from src.deepthrottle.algorithms.token_bucket import TokenBucket
 
 
 class AlgorithmType(Enum):
@@ -10,24 +10,29 @@ class AlgorithmType(Enum):
     TOKEN_BUCKET = TokenBucket
     SLIDING_WINDOW_COUNTER = SlidingWindowCounter
 
-
 class RateLimiter:
-    def __init__(
+    def __init__ (
         self,
         algorithm: AlgorithmType,
-        rate: float,
         capacity: int,
+        rate: float | None = None,
         window_size_seconds: float | None = None,
     ):
-        self.algorithm: AlgorithmType = algorithm
-        self.capacity: float | int = capacity
-        self.window_size_seconds: float | None = window_size_seconds
-        self.rate: float = rate
+        self.algorithm: AlgorithmType = algorithm 
+        self.capacity = capacity
+        self.rate = rate
+        self.window_size_seconds = window_size_seconds
 
-        if window_size_seconds is not None:
-            self._algorithm_instance = algorithm.value(capacity, window_size_seconds)
-        else:
-            self._algorithm_instance = algorithm.value(capacity, rate)
+        if algorithm == AlgorithmType.SLIDING_WINDOW_COUNTER:
+            if window_size_seconds is None:
+                raise ValueError("SlidingWindowCounter requires 'window_size_seconds'.")
+            self._algorithm_instance = algorithm.value(max_requests=capacity, window_size_seconds=window_size_seconds)
+
+        elif algorithm in (AlgorithmType.TOKEN_BUCKET, AlgorithmType.LEAKY_BUCKET):
+            if rate is None:
+                raise ValueError(f"{algorithm.name} requires 'rate'.")
+            self._algorithm_instance = self.algorithm.value(capacity, rate)
+            
 
     def allow(self, key: str) -> tuple[float | int, bool, float | None]:
         tokens: float | int
@@ -37,6 +42,5 @@ class RateLimiter:
         tokens, allowed, wait_time = self._algorithm_instance.add_request()
         return tokens, allowed, wait_time
 
-
-    def get_state(self, key: str) -> dict[str, float | str]:
+    def get_state(self) -> dict[str, float | str]:
         return self._algorithm_instance.get_state()
